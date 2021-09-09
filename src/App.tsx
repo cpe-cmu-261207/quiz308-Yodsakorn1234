ad594e2db8a9f7da2bd7d76add8195a137a436a4
Yodsakorn <menming1234@outlook.com>
2021-09-09 16:29:58 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { comments } from './data/comments';
-import Post from './data/Post';
-import { get } from 'http';
+import { comments, CommentType } from './data/comments';
 
 function App() {
-  function render(){
-     comments.map((x,key)=>{
-       return <Post  key={key} username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies} />
-     })
+  const generateReplies = (replies: CommentType[]) => {
+    return (replies.map(x =>
+      <div className="flex p-2 items-start space-x-2 pl-14">
+        <img className="w-10 w-10 rounded-full" src={x.userImagePath}></img>
+        <div className="bg-gray-500 rounded-lg p-2">
+          <p className="font-semibold text-white">{x.username}</p>
+          <p className='text-white'>{x.commentText}</p>
+          {x.likeNum > 0 ?
+            <div className='flex items-center'>
+              <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+              <p className='text-gray-300'>{x.likeNum}</p>
+            </div> : null}
+        </div>
+      </div>
+    ))
   }
+
+
   return (
     <div className="p-2">
       {/* post container */}
@@ -21,24 +32,46 @@ function App() {
 
           {/* image and name */}
           <div className="flex space-x-2 items-center">
-            <img className="w-12 h-12 rounded-full" src="/img/Pzean.jpg"></img>
-            <span className='font-semibold text-lg text-white'>Yodsakorn Likitrungson 630612109</span>
+            <img className="w-12 h-12 rounded-full" src="/profileImages/raiden.jpg"></img>
+            <span className='font-semibold text-lg text-white'>Yodsakorn likitrungson 630612109</span>
           </div>
 
           {/* status message */}
-          <p className='text-white'>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</p>
+          <p className='text-white'>Quiz ยากมากครับ ไม่ไหวแล้ว #261207</p>
 
           {/* like section */}
           <div className='flex items-center'>
             <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-            <p className='text-gray-300'>100 คน</p>
+            <p className='text-gray-300'>999 คน</p>
           </div>
 
         </div>
 
         {/* comments section */}
-        {render()}
-        
+        <div className="">
+          {comments.map(x =>
+            <div>
+              <div className="flex p-2 items-start space-x-2">
+                <img className="w-10 w-10 rounded-full" src={x.userImagePath}></img>
+                <div className="bg-gray-500 rounded-lg p-2">
+                  <p className="font-semibold text-white">{x.username}</p>
+                  <p className='text-white'>{x.commentText}</p>
+                  {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                  {x.likeNum > 0 ?
+                    <div className='flex items-center'>
+                      <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                      <p className='text-gray-300'>{x.likeNum}</p>
+                    </div> : null}
+                </div>
+              </div>
+              {generateReplies(x.replies)}
+            </div>
+          )}
+
+
+
+        </div>
+
       </div>
     </div>
   );
